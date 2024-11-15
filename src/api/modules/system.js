369cac84461d7b75de808ee17137bcbd0017b0a9
gitboyzcf <boyzcf@qq.com>
2024-11-15 17:41:51 +0800

    Update system.js

--- src/api/modules/system.js
+++ src/api/modules/system.js
@@ -2,7 +2,7 @@ import { request } from '@/api/request.js'
 export default {
   API_GET_GEO_JSON_GET(params = {}) {
     return request({
-      url: `/geojson/${params.code}_full.json`,
+      url: `/vue3-simple-screen/geojson/${params.code}_full.json`,
       method: 'get'
     })
   }
